846d9014df2dc9e29edcd427ddae3c4a3b25178a
Vivian Mburu <105209101+YrimuhMburu@users.noreply.github.com>
2023-02-18 20:46:41 +0300

    Add files via upload

---
+++ categorize-values.py
@@ -0,0 +1,3 @@
+myMixedTypeList = [ 45, 50, 1.02, 70.90, "my cat", "my car"]
+for item in myMixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
---
+++ conditionals2.py
@@ -0,0 +1,16 @@
+userReply = input(" Do you need to ship a package (Enter yes or no)")
+if userReply == "yes" :
+  print(" we can help you ship the package")
+else :
+  print("please come back again when you need to ship a package")
+userReply = input("Would you like to buy stamps, buy envelopes or  make a copy? (Enter, stamps, envelopes or copy)")
+if userReply == "stamps":
+   print("we have  stamp designs from")
+elif userReply == "envelopes":
+    print("we have different size of envelopes to choose from")
+elif userReply == "copy":
+    copies = input("How many copies would you like to make? (Enter a number of copies)")
+    print(" Here are {} copies" .format(copies))
+else:
+    print("thank you and come again")
+  
